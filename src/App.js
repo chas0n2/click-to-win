1ba9f574492f881ef71e7eb1272232272fcaa569
chason <chasonlsmith@gmail.com>
2020-01-22 16:40:10 -0700

    handled onclick for win and reset.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import ImageCard from './components/Image-Card';
+import data from './images.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    data,
+    score: 0,
+    images: [],
+    clicked: 0
+  }
+  // clickEvent
+
+  handleClickEvent = (id) => {
+    this.shuffle();
+    this.clicked(id);
+  }
+
+  shuffle = () => {
+    let shuffleImage = this.state.data;
+    shuffleImage = shuffleImage.sort(() => Math.random() - 0.42);
+
+    this.setState({
+      images: shuffleImage
+    })
+
+  }
+
+  clicked = (id) => {
+
+    let clicked = this.state.clicked;
+    this.setState({
+      clicked: id
+
+    })
+    console.log(clicked)
+    console.log(id)
+
+    if(id === clicked) {
+      alert("correct!")
+      let newscore = this.state.score
+      newscore = newscore + 1
+      this.setState({
+        score: newscore,
+        clicked: 0
+      })
+    }
+    else if (clicked === 0) {
+      console.log("do nothing")
+    }
+    else
+    {
+      alert("incorrect")
+      this.setState({
+        score: 0,
+        clicked: 0
+      })
 }
+
+if ( this.state.score === 5 ) {
+  alert("Nice... But don't get cocky kid!")
+  this.setState({
+    score: 0,
+    clicked: 0
+  })
+}
+
+render () {
+  return (
+      <div>
+          <Header score = {this.state.score} />
+          <div>
+              {this.state.data.map(item => (
+                  <ImageCard
+                      key={item.id}
+                      id={item.id}
+                      clicked={this.handleClickEvent}
+                      img= {item.img}
+                  />
+              ))
+}
+          </div>
+      </div>
+  )}
+
+
+};
 
 export default App;
