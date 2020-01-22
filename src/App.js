122fa99d89b056a6974ae55f95ff6a6e192c2b97
chason <chasonlsmith@gmail.com>
2020-01-22 16:45:49 -0700

    fixed missing brackets.

--- src/App.js
+++ src/App.js
@@ -1,89 +1,93 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import ImageCard from './components/Image-Card';
-import data from './images.json';
+import ImageCard from './components/ImageCard';
+import data from "./images.json";
+
 
 
 class App extends Component {
-  state = {
-    data,
-    score: 0,
-    images: [],
-    clicked: 0
-  }
-  // clickEvent
+    state = {
+      data,
+      score: 0,
+      images: [],
+      clicked: 0
+}
 
-  handleClickEvent = (id) => {
+handleClickEvent = (id) => {
     this.shuffle();
     this.clicked(id);
-  }
+}
 
-  shuffle = () => {
-    let shuffleImage = this.state.data;
-    shuffleImage = shuffleImage.sort(() => Math.random() - 0.42);
+shuffle = () => {
+    let shuffledImages = this.state.data;
+    shuffledImages = shuffledImages.sort(() => Math.random() - 0.42);
 
     this.setState({
-      images: shuffleImage
+        images: shuffledImages
     })
+}
 
-  }
-
-  clicked = (id) => {
+clicked = (id) => {
 
     let clicked = this.state.clicked;
-    this.setState({
-      clicked: id
 
+    this.setState({
+        clicked: id
     })
+
     console.log(clicked)
     console.log(id)
 
     if(id === clicked) {
-      alert("correct!")
-      let newscore = this.state.score
-      newscore = newscore + 1
-      this.setState({
-        score: newscore,
-        clicked: 0
-      })
+        alert("correct")
+        let newscore = this.state.score
+        newscore = newscore + 1
+        this.setState({
+            score: newscore,
+            clicked: 0
+        })
+
     }
-    else if (clicked === 0) {
-      console.log("do nothing")
+     else if (clicked === 0) {
+        console.log("do nothing")
     }
     else
     {
-      alert("incorrect")
-      this.setState({
-        score: 0,
-        clicked: 0
-      })
-}
-
-if ( this.state.score === 5 ) {
-  alert("Nice... But don't get cocky kid!")
-  this.setState({
-    score: 0,
-    clicked: 0
-  })
+        alert("incorrect")
+        this.setState({
+            score: 0,
+            clicked: 0
+        })
+    }
+    
+    if( this.state.score === 5) {
+        alert("Nice... But don't get cocky kid!")
+        this.setState({
+            score: 0,
+            clicked: 0
+        })
+    }
+    
 }
 
 render () {
-  return (
-      <div>
-          <Header score = {this.state.score} />
-          <div>
-              {this.state.data.map(item => (
-                  <ImageCard
-                      key={item.id}
-                      id={item.id}
-                      clicked={this.handleClickEvent}
-                      img= {item.img}
-                  />
-              ))
-}
-          </div>
-      </div>
-  )}
+    return (
+        <div>
+            <Header score = {this.state.score}/>
+            <div>
+                {this.state.data.map(item => (
+                    <ImageCard
+                        key={item.id}
+                        id={item.id}
+                        clicked={this.handleClickEvent}
+                        img= {item.img}
+                    />
+                ))
+                }
+            </div>
+        </div>
+    );
+};
 
 
 };
