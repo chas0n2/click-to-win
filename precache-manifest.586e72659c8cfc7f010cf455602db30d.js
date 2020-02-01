edf066a377c55a37a8139cf4214ed28b4c2a0dfa
chason <chasonlsmith@gmail.com>
2020-01-31 19:05:33 -0700

    Updates

---
+++ precache-manifest.586e72659c8cfc7f010cf455602db30d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb21a68503a854c840510a3f6888b57",
+    "url": "/clicktowin/index.html"
+  },
+  {
+    "revision": "eb97db0a86be4b1392df",
+    "url": "/clicktowin/static/css/main.e45e0bde.chunk.css"
+  },
+  {
+    "revision": "6baab0a55f6ff1be51d9",
+    "url": "/clicktowin/static/js/2.81b75c04.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicktowin/static/js/2.81b75c04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb97db0a86be4b1392df",
+    "url": "/clicktowin/static/js/main.222e9802.chunk.js"
+  },
+  {
+    "revision": "b66f1e36e8ae5f38849d",
+    "url": "/clicktowin/static/js/runtime-main.3e58b81a.js"
+  }
+]);
